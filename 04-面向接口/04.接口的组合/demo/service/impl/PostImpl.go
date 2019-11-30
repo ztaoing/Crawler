e89e83fb821984bc7d1f1c7f5460ea1d5a6f1547
tao <1216707759@qq.com>
2019-11-30 22:02:29 +0800

    go crawler

---
+++ 04-面向接口/04.接口的组合/demo/service/impl/PostImpl.go
@@ -0,0 +1,8 @@
+package impl
+
+type PostImpl struct {
+}
+
+func (PostImpl) Post(url string, form map[string]string) string {
+	return " "
+}
---
+++ 04-面向接口/04.接口的组合/demo/service/impl/RetrieverPosterImpl.go
@@ -0,0 +1,12 @@
+package impl
+
+type RetrieverPosterImpl struct {
+}
+
+func (*RetrieverPosterImpl) Get(url string) string {
+	return "get done"
+}
+
+func (*RetrieverPosterImpl) Post(url string, form map[string]string) string {
+	return "post done"
+}
